62c1fcbc8ed57b959b36be6e6389eddfba20bd84
Tylar <murray.tylar@gmail.com>
2017-11-07 10:45:43 -0500

    Create wv2-processing.py

---
+++ wv2-processing.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# NOTE: use python3 in the shebang line above if targeting py3 and not py2-compatible
+""" example main file with cmd line interface """
+
+from argparse import ArgumentParser
+
+from wv2_processing import main
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='short desc of projname goes here')
+
+    parser.add_argument("source", help="directory to copy from")
+    parser.add_argument('-l', '--log',
+        help="desired filepath of log file",
+        default="/var/opt/projectname/backup.log"
+    )
+    parser.add_argument('--rclonelog',
+        help="desired path of rclone log file",
+        default=None
+    )
+    parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                        action="count",
+                        default=0
+    )
+
+    args = parser.parse_args()
+
+    main(args)
