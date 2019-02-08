f6f82018370fd2e9f009b28af4c44e3041667729
Tylar Murray <murray.tylar@gmail.com>
2019-02-08 17:10:11 +0000

    update setup & + reqs.txt files

--- setup.py
+++ setup.py
@@ -4,9 +4,12 @@
 from setuptools import setup
 import io
 
-VERSION='0.0.1'
+VERSION = '0.0.1'  # should match __version__ in PROJECT_NAME.__init__.py
 
+
+# === long_description comes from README.md
 def read(*filenames, **kwargs):
+    """Helper fn to help get project info for setup()"""
     encoding = kwargs.get('encoding', 'utf-8')
     sep = kwargs.get('sep', '\n')
     buf = []
@@ -15,15 +18,35 @@ def read(*filenames, **kwargs):
             buf.append(f.read())
     return sep.join(buf)
 
-long_description = read('README.md') #, 'CHANGES.txt')
+long_description = read('README.md')  # , 'CHANGES.txt')
 
-setup(name='wv2_processing',
-    version=VERSION,
+# === installation requirements read from requirements files
+# so these two commands are equivalent:
+# * `pip install .`
+# * `pip install -r requirements.txt`
+_tests_require = [
+    line.strip() for line in open('requirements_tests.txt')
+    if line.strip() and not line.strip().startswith('--')
+]
+
+_install_requires = [
+    line.strip() for line in open('requirements.txt')
+    if line.strip() and not line.strip().startswith('--')
+]
+
+_extras_require = {  # allows install w/ `pip install .[test]`
+    'test': _tests_require
+}
+
+setup(
+    name='wv2_processing',
     description='IMaRS WV2 classification processing scripts',
-    long_description=long_description,
     author='Matthew McCarthy',
     author_email='mjm8@mail.usf.edu',
     url='https://github.com/usf-imars/wv2-processing',
-    tests_require=['nose'],
-    install_requires=['gdal']
+    version=VERSION,
+    long_description=long_description,
+    install_requires=_install_requires,
+    tests_require=_tests_require,
+    extras_require=_extras_require,
 )
