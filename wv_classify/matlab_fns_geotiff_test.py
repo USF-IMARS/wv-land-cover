d5c865efe97f9c1cb4521ac8d0d2476c3c8257a9
Tylar Murray <murray.tylar@gmail.com>
2019-02-11 18:43:57 +0000

    + geotif cp test

---
+++ wv_classify/matlab_fns_geotiff_test.py
@@ -0,0 +1,17 @@
+# std modules:
+from unittest import TestCase
+import filecmp
+
+from matlab_fns import geotiffread
+from matlab_fns import geotiffwrite
+
+
+class Test_geotiff_io(TestCase):
+    def test_geotiff_cp(self):
+        """read then write geotiff; output matches input."""
+        INFILEPATH = "test/WV_Testing/Rrs/16FEB12162517-M1BS-_RB_Rrs.tif"
+        OUTFILEPATH = "test/cp_test.tif"
+        data_array, data_obj = geotiffread(INFILEPATH)
+        geotiffwrite(OUTFILEPATH, data_array, data_obj, 4326)
+        self.assertTrue(filecmp.cmp(INFILEPATH, OUTFILEPATH))
+        # TODO: cleanup rm OUTFILEPATH
