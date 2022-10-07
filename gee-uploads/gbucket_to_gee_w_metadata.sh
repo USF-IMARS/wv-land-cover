635041001ab42eead6342a512107017fca0d553c
Tylar <murray.tylar@gmail.com>
2022-10-07 14:32:22 -0400

    improve formatting, doc. add todo for xml reading

--- gee-uploads/gbucket_to_gee_w_metadata.sh
+++ gee-uploads/gbucket_to_gee_w_metadata.sh
@@ -1,35 +1,71 @@
 #!/usr/bin/env bash
 # Usage:
-# ./transfer_seagrass.sh src_bucket dest_asset
-# Example: 
-# ./transfer_seagrass.sh seagrass_mosaics/original_mosaics users/lizcanosandoval/Seagrass/Sentinel/01_OriginalMosaics
-# ./transfer_seagrass.sh seagrass_mosaics/edited_mosaics users/lizcanosandoval/Seagrass/Sentinel/02_EditedMosaics
-# Modified from: https://www.tucson.ars.ag.gov/notebooks/uploading_data_2_gee.html#
+# ./gbucket_to_gee_w_metadata.sh src_bucket xml_filespath dest_asset
+#
+# Moves .tif files from a GCloud bucket into GEarthEngine including relevant metadata.
+# Some metadata is hard-coded in the file below and some metadata is pulled from the 
+# `.xml` file that corresponds to each `.tif`.
+# The `.xml` file must be on the local machine and have the same filename as the
+# `.tif` file in the GCloud bucket.
+#
+# example GCloud filename:
+# 	20200929T162717_03_P008_WV03_ClassificMap_fullClass_Rookery.tif
+# example .NTF file from which the .tif was generated:
+#   20SEP29162717-M1BS-504649660010_03_P008.NTF
+# corresponding .xml filename:
+#   20SEP29162717-M1BS-504649660010_03_P008.XML
+#
+# Examples: 
+# ./gbucket_to_gee_w_metadata.sh \
+#		seagrass_mosaics/original_mosaics \
+# 		./seagrass_mosiacs/xml_files/ \
+#		users/lizcanosandoval/Seagrass/Sentinel/01_OriginalMosaics
+#
+# ./gbucket_to_gee_w_metadata.sh \
+# 		rookery-wv-classmaps \
+#		/srv/imars-objects/rookery/Processed/wv_classMaps_rgb \       
+#
+# Modified from: https://www.tucson.ars.ag.gov/notebooks/uploading_data_2_gee.html
 
-##Some metadata
+# hardcoded metadata
 country="USA"
-satellite="Sentinel-2"
-generator="Lizcano-Sandoval"
-classifier="SVM"
+satellite="WV0(2|3)"
+generator="Tylar Murray & Digna Rueda"
+classifier="NERRS-mangroves-decision-tree"
 
+# create the collection
 result=`earthengine create collection $2`
-if `test -z "$result"`; then
+if `test -z "$result"`; then  # exit if creation failed
     echo $result
     exit 1
 fi
-# In the following loop we get the entire path to all the geotifs using the specified 
-# Gcloud bucket. Each file will have a format like this: gs://my_gee_bucket/FILE_January2000.tif
-# Each call to earthengine it will launch a task that you can monitor in the JS Code editor 
-# at the "tasks" tab.
+
+# TODO: implement .xml file reading using https://github.com/USF-IMARS/wv-land-cover/blob/master/wv_classify/read_wv_xml.py
+echo xml reading not yet implemented
+exit 1
+
+# In the following loop we get the entire path to all the geotifs using the specified Gcloud bucket. 
+# Each file will have a format like this: `gs://my_gee_bucket/FILE_January2000.tif`.
+# Each call to earthengine will launch a task that you can monitor in the JS Code editor "tasks" tab.
 for geotiff in `gsutil ls gs://$1/*.tif`; do  
     #filename=${geotiff%.*}
-	filename=${geotiff##*/} 
+    filename=${geotiff##*/} 
     asset_id="${filename%.*}" 
 	echo "*** Transfering file " $asset_id "***"
+	
 	year=${asset_id:6:4}
 	date="${year}-01-01T12:00:00"
 	tile=${asset_id:0:6}
 	tile_id="${tile%_*}" 
 	code=${asset_id:11:4}
-    earthengine upload image gs://$1/$filename -f --asset_id=$2/$asset_id --nodata_value=0 --crs="EPSG:4326" -ts=$date -p="year=${year}" -p="name_code=${code}" -p="tile_id=${tile_id}" -p="country=${country}" -p="satellite=${satellite}" -p="generator=${generator}" -p="classifier=${classifier}"
+    
+	earthengine upload image gs://$1/$filename -f --asset_id=$2/$asset_id \
+		--nodata_value=0 --crs="EPSG:4326" -ts=$date \
+		-p="year=${year}" \
+		-p="name_code=${code}" \
+		-p="tile_id=${tile_id}" \
+		-p="country=${country}" \
+		-p="satellite=${satellite}" \
+		-p="generator=${generator}" \
+		-p="classifier=${classifier}"
 done
