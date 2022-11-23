b56e88c598ee7c2b295b3826d5a366f895195c31
Tylar <email@tylar.info>
2022-11-23 14:17:57 -0500

    + digna's submission script

---
+++ slurm_submission_scripts/submit_py_ortho_jobos_year2013.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#SBATCH --partition=circe
+#SBATCH --job-name ="Jobos-per-year"
+#SBATCH --nodes=1
+##SBATCH --ntasks-per-node=4
+#SBATCH --mem-per-cpu=52240
+#SBATCH --time=40:00:00
+#SBATCH --array=0-1
+#SBATCH --output=/work/d/druedaro/wv2_scripts/slurm_submission_scripts/SlurmOutput/output.%j.txt
+## Can submit up to 10,000 jobs at once, but only 512 will run concurrently
+## SBATCH --mail-type=ALL
+## SBATCH --mail-user=druedaro@usf.edu
+
+
+#module purge
+module add apps/python/2.7.5
+# module add apps/gdal/3.0.1
+
+# Python code to check processing time:
+#    starttime = datetime.today()
+#    LogMsg('Image: %s' %(info.srcfn))
+
+## Setup input arguments & file locations
+images1=`ls /work/d/druedaro/img/Jobos_perYear/Jobos_2013b/*.[nN][tT][fF]`
+met=`ls /work/d/druedaro/img/Jobos_perYear/Jobos_2013b/*.[xX][mM][lL]`
+ortho_out=/work/d/druedaro/output/Ortho/Jobos_perYear/Jobos_2013b/
+rrs_out=/work/d/druedaro/output/Rrs/Jobos_perYear/Jobos_2013b/
+
+
+# Setup Matlab arguments
+crd_sys=EPSG:4326
+areaName='Jobos'
+
+## Run Python code
+images1a=($images1)
+image=${images1a[$SLURM_ARRAY_TASK_ID]}
+
+python /work/d/druedaro/wv2_scripts/pgc_duplication/pgc_ortho.py -p 4326 -c ns -t UInt16 -f GTiff --no_pyramids $image $ortho_out
+
+## Run Matlab code
+module add apps/matlab/r2018a
+
+input_img_basename=$(basename "${image%.[nN][tT][fF]}")
+echo $input_img_basename
+image2="$ortho_out${input_img_basename}_u16ns4326.tif"
+echo $image2
+met=($met)
+met=${met[$SLURM_ARRAY_TASK_ID]}
+
+# matlab running
+matlab -nodisplay -nodesktop -r "WV_Clasific_cleaner_v4_Jobos.m('$image2','$met','crd_sys','$areaName','$rrs_out','$SLURM_ARRAY_TASK_ID')"
+
+
+# other_ortho_fpath="$ortho_out${input_img_basename}_u16ns4326.prj"
+# rm $image2 
+# rm $other_ortho_fpath
+
+
+    #### Calculate Total Time
+ #   endtime = datetime.today()
+ #   td = (endtime-starttime)
+ #   LogMsg("Total Processing Time: %s\n" %(td))
+ 
+ 
+ 
+
